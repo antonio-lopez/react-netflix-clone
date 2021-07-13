83916d1e3f50cbc96ca0915b45e82ac079edf845
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-07-13 13:27:39 -0700

    update background position of image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -30,7 +30,7 @@ const Banner = () => {
       style={{
         backgroundSize: 'cover',
         backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
-        backgroundPosition: 'center center',
+        backgroundPosition: 'top center',
       }}
     >
       <div className='banner__contents'>
